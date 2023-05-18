b8d8ec35c171526b23aeba81f075e5cb52e70879
antoniobmn <antoniobmn2@gmail.com>
2023-05-18 09:31:32 -0300

    creating folder if it doesn't exists

--- src/services/fileServices.js
+++ src/services/fileServices.js
@@ -1,4 +1,9 @@
+const fs = require("fs");
+
 const saveFile = async (file) => {
+  if (!fs.existsSync("./uploads/")) {
+    fs.mkdirSync("./uploads/");
+  }
   file.mv(`./uploads/${file.name}`, (err) => {
     if (err) throw err;
   });
